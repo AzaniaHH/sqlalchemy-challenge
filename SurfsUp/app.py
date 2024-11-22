f7cc383b1615c667d9f7d147d8b8bce868c010f9
AzaniaHH <azaniahoodhosten@gmail.com>
2024-11-21 19:33:48 -0600

    Commit and save changes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,28 +1,33 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine("sqlite:///SurfsUp/Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
-
 
 
 #################################################
