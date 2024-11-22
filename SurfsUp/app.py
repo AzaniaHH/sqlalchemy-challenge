9dbea80dd10f2943077c11342490455d07eb1639
AzaniaHH <azaniahoodhosten@gmail.com>
2024-11-21 19:34:24 -0600

    Save changes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -33,3 +33,73 @@ session = Session(engine)
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    """Homepage listing all routes."""
+    return (
+        f"Welcome to the Climate App!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the last 12 months of precipitation data as JSON."""
+    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    latest_date = datetime.strptime(latest_date, "%Y-%m-%d")
+    one_year_ago = latest_date - timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
+
+    precipitation_data = {date: prcp for date, prcp in results}
+    return jsonify(precipitation_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of stations."""
+    results = session.query(Station.station).all()
+    stations_list = [station[0] for station in results]
+    return jsonify(stations_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return temperature observations for the most active station in the last year."""
+    most_active_station = session.query(Measurement.station).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).first()[0]
+    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    latest_date = datetime.strptime(latest_date, "%Y-%m-%d")
+    one_year_ago = latest_date - timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date >= one_year_ago).all()
+
+    temperatures = [temp[0] for temp in results]
+    return jsonify(temperatures)
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_stats(start, end=None):
+    """Return TMIN, TAVG, and TMAX for a given start or start-end range."""
+
+    if end:
+        results = session.query(
+            func.min(Measurement.tobs),
+            func.avg(Measurement.tobs),
+            func.max(Measurement.tobs)
+        ).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    else:
+        results = session.query(
+            func.min(Measurement.tobs),
+            func.avg(Measurement.tobs),
+            func.max(Measurement.tobs)
+        ).filter(Measurement.date >= start).all()
+
+    temp_stats = list(results[0])
+    return jsonify(temp_stats)
+
+if __name__ == "__main__":
+    app.run(debug=True)
